f393f0037157d3aaca7b9a50d72f0166d2ed3274
goldenwaffles99 <96534411+goldenwaffles99@users.noreply.github.com>
2021-12-22 10:59:26 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (99999999999999999999999999999999999999999999999999999999999999999999999999 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) {
+        alert('you really have enough.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
